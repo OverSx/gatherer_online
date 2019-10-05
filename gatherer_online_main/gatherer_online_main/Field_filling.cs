36b453ac9fb85156472026534b6d1e0ebff931c0
OverSx <32136020+OverSx@users.noreply.github.com>
2019-10-05 13:57:27 +0300

    Initialization of Field_filling class
    Initialize new class Field_filling

---
+++ gatherer_online_main/gatherer_online_main/Field_filling.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gatherer_online_main
+{
+    class Field_filling
+    {
+        // 0 - empty
+        // 1 - goal
+        // 2 - final goal
+        // 3 - obstacle cell
+        // 4 - agent
+
+        int[,] field_size;
+        int numGoals;
+        int numObstacles;
+
+        Random rand = new Random();
+        int[,] filled_field;
+        public Field_filling(int field_w, int field_h, int field_s, int field_g)
+        {
+            field_size = new int[field_w, field_h];
+            numGoals = field_g;
+            numObstacles = field_s;
+
+            for (int i = 0; i <= field_w; i++)
+            {
+                for (int j = 0; j <= field_h; j++)
+                {
+                    filled_field = [i]
+                }
+            }
+        }
+    }
+}
