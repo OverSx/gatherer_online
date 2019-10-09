dddb12f1005f733be9ed6b84f223cb5b15c23139
OverSx <32136020+OverSx@users.noreply.github.com>
2019-10-09 13:44:39 +0300

    Some protect to textboxes.
    Only numeric char can be added to textboxes.

--- gatherer_online_main/gatherer_online_main/MainWindow.xaml.cs
+++ gatherer_online_main/gatherer_online_main/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace gatherer_online_main
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool IsNumber = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,5 +33,111 @@ namespace gatherer_online_main
             Field new_field = field.Generate_field();
             new_field.ShowDialog();
         }
+
+        private void tb_width_KeyDown(object sender, KeyEventArgs e)
+        {
+            IsNumber = false;
+            if (e.Key < Key.D0 || e.Key > Key.D9 || e.Key == Key.Back)
+            {
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                {
+                    IsNumber = true;
+                }
+            }
+            if (e.Key == Key.Space)
+            {
+
+            }
+            if (IsNumber)
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void tb_height_KeyDown(object sender, KeyEventArgs e)
+        {
+            IsNumber = false;
+            if (e.Key < Key.D0 || e.Key > Key.D9 || e.Key == Key.Back)
+            {
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                {
+                    IsNumber = true;
+                }
+            }
+            if (e.Key == Key.Space)
+            {
+
+            }
+            if (IsNumber)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tb_stop_KeyDown(object sender, KeyEventArgs e)
+        {
+            IsNumber = false;
+            if (e.Key < Key.D0 || e.Key > Key.D9 || e.Key == Key.Back)
+            {
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                {
+                    IsNumber = true;
+                }
+            }
+            if (e.Key == Key.Space)
+            {
+
+            }
+            if (IsNumber)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tb_goals_KeyDown(object sender, KeyEventArgs e)
+        {
+            IsNumber = false;
+            if (e.Key < Key.D0 || e.Key > Key.D9 || e.Key == Key.Back)
+            {
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                {
+                    IsNumber = true;
+                }
+            }
+            if (e.Key == Key.Space)
+            {
+
+            }
+            if (IsNumber)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tb_width_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tb_width.Text = tb_width.Text.Replace(" ", "");
+            tb_width.SelectionStart = tb_width.Text.Length;
+        }
+
+        private void tb_height_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tb_height.Text = tb_height.Text.Replace(" ", "");
+            tb_height.SelectionStart = tb_height.Text.Length;
+        }
+
+        private void tb_stop_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tb_stop.Text = tb_stop.Text.Replace(" ", "");
+            tb_stop.SelectionStart = tb_stop.Text.Length;
+        }
+
+        private void tb_goals_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tb_goals.Text = tb_goals.Text.Replace(" ", "");
+            tb_goals.SelectionStart = tb_goals.Text.Length;
+        }
     }
 }
+
