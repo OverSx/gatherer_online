b02752be5e18cf4dce583b4302e46cc03bba450d
OverSx <32136020+OverSx@users.noreply.github.com>
2019-10-09 18:15:06 +0300

    New func FindMyAgent
    Added a func, which will find current Agent coordinates.

--- gatherer_online_main/gatherer_online_main/Plotter.cs
+++ gatherer_online_main/gatherer_online_main/Plotter.cs
@@ -10,9 +10,39 @@ namespace gatherer_online_main
     {
         int[,] field_plotter;
 
+        public Plotter(int[,] field)
+        {
+            field_plotter = field;
+        }
+
         public void Let_find_way()
         {
 
+        }
+        private List<int> FindMyAgent()
+        {
+            List<int> AgentCoord = new List<int>();
+            bool AgentCoordIsFind = false;
+
+            for (int i = 0; i < field_plotter.GetLength(0); i++)
+            {
+                for (int j = 0; j < field_plotter.GetLength(1); j++)
+                {
+                    if (field_plotter[i, j] == 4)
+                    {
+                        AgentCoord[0] = i;
+                        AgentCoord[1] = j;
+                        AgentCoordIsFind = true;
+                        break;
+                    }
+                }
+                if (AgentCoordIsFind)
+                {
+                    break;
+                }
+            }
+
+            return AgentCoord;
         }
     }
 }
